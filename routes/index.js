fefb287bc01f40ec67436399a621c3f4a2f7130b
haku-noir <hakunoir.00@gmail.com>
2019-09-06 16:32:16 +0900

    add: スタートページの追加

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Linux Dungeon' });
+  res.render('index');
 });
 
 router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Linux Dungeon' });
+  res.render('index');
 });
 
 module.exports = router;

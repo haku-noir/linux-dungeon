d6d0e8eca79c00a7b5fa2dfe5b28aa1462a923a1
nununugit <hiraganadenu@gmail.com>
2019-09-11 01:18:51 +0900

    add: データベースへ接続

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,19 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
 router.post('/', function(req, res, next) {
-  user = req.body.user;
-  pass = req.body.pass;
+    user = req.body.user;
+    pass = req.body.pass;
 
-  if(req.body.registration){
-    console.log(`REGISTRATION user: ${user}, pass:${pass}`);
-  }else{
-    console.log(`LOGIN user: ${user}, pass:${pass}`);
-  }
-  res.render('start', {user});
+    const db = mysql.createConnection({
+        host: process.env.DATABASE_HOST || 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'userdata'
+    });
+    db.connect(error => {
+        if (error) {
+            console.error('error connecting: ' + error.stack);
+            return;
+        }
+        console.log('connected as id ' + db.threadId);
+    });
+
+    if (req.body.registration) {
+        console.log(`REGISTRATION user: ${user}, pass:${pass}`);
+    } else {
+        console.log(`LOGIN user: ${user}, pass:${pass}`);
+    }
+    res.render('start', { user });
 });
 
 module.exports = router;

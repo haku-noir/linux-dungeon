c20a6e0b07d49d47c6af3973711a945ca1091f4a
nununugit <hiraganadenu@gmail.com>
2019-09-11 10:00:05 +0900

    add: ユーザ登録の処理は可能

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -15,6 +17,7 @@ router.post('/', function(req, res, next) {
         password: 'password',
         database: 'userdata'
     });
+
     db.connect(error => {
         if (error) {
             console.error('error connecting: ' + error.stack);
@@ -23,10 +26,32 @@ router.post('/', function(req, res, next) {
         console.log('connected as id ' + db.threadId);
     });
 
+
     if (req.body.registration) {
-        console.log(`REGISTRATION user: ${user}, pass:${pass}`);
+        try {
+            db.query("INSERT INTO user_pass (user,pass) values( ? , ? );", [user, pass], (error, result) => {
+                console.log(result);　
+                res.render('start', { user });
+            })
+        } catch (e) {
+            res.send('登録失敗');
+        }
     } else {
         console.log(`LOGIN user: ${user}, pass:${pass}`);
+        try {
+            db.query("SELECT FROM user_pass where user=? AND pass=?;", [user, pass], (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                console.log(result);　
+                if (result[0]['user']) {
+                    res.render('start', { user });
+                }
+            })
+        } catch (e) {
+            res.send('ログイン失敗');
+            res.redirect('login');
+        }
     }
     res.render('start', { user });
 });
--- db/userdata.sql
+++ db/userdata.sql
@@ -4,3 +4,7 @@ CREATE TABLE user_pass (
   user  VARCHAR(20),
   pass  VARCHAR(20)
 );
+
+
+
+INSERT INTO user_pass (user,pass) values('test','test');

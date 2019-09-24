aee8cab32db1c08211930cfa2798f9fc22b0cfca
nununugit <hiraganadenu@gmail.com>
2019-09-24 17:01:29 +0900

    add: DBにuidを追加

--- db/userdata.sql
+++ db/userdata.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE userdata;
+
 USE userdata;
-CREATE TABLE user_pass (
-  user  VARCHAR(20),
-  pass  VARCHAR(20)
-);
+CREATE TABLE user_pass(
+    uid INT auto_increment NOT NULL PRIMARY KEY,
+    user  VARCHAR(20),
+    pass  VARCHAR(20));
 
 INSERT INTO user_pass (user,pass) values('test','test');
+

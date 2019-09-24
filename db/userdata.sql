59eca2ec7c2bb6cbddd92937051e60660a62e9c5
haku-noir <hakunoir.00@gmail.com>
2019-09-24 23:21:33 +0900

    hotfix: DBが起動しないバグの修正

--- db/userdata.sql
+++ db/userdata.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE userdata;
-
 USE userdata;
+
 CREATE TABLE user_pass(
     uid INT auto_increment NOT NULL PRIMARY KEY,
     user  VARCHAR(20),
     pass  VARCHAR(20));
 
 INSERT INTO user_pass (user,pass) values('test','test');
-

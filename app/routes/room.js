969beae302d107e81b6be2cbc8c522c4dca38401
haku-noir <hakunoir.00@gmail.com>
2019-09-28 17:33:36 +0900

    add: 問題ページに解答機能を追加
    解答すると解答ページで正解or不正解を表示する。

--- app/routes/room.js
+++ app/routes/room.js
@@ -4,14 +4,25 @@ var db = require('../my_modules/db');
 
 router.post('/', function(req, res, next) {
   const did = req.body.did;
-  db.query("SELECT q.qid, q.path, q.que FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", [did], (err, rows) => {
+
+  db.query("SELECT q.qid, q.path, q.que FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
     console.log(rows[0]);
     res.render('question', {qid: rows[0].qid, path: rows[0].path, que: rows[0].que});
   });
 });
 
 router.post('/answer', function(req, res, next) {
-  res.render('answer-correct');
+  const qid = req.body.qid;
+  const ans = req.body.ans;
+
+  db.query("SELECT ans FROM questions_datas WHERE qid = ?;", qid, (err, rows) => {
+    console.log(rows[0]);
+    if(rows[0].ans === ans){
+      res.render('answer-correct');
+    }else{
+      res.render('answer-incorrect');
+    }
+  });
 });
 
 module.exports = router;

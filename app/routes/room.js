effe5efc5632ee7190a8269e0b1bbb676a87fe73
haku-noir <hakunoir.00@gmail.com>
2019-09-28 19:28:07 +0900

    change: 問題ページと解答ページ間における問題の伝達をqidからuidに変更

--- app/routes/room.js
+++ app/routes/room.js
@@ -5,22 +5,22 @@ var db = require('../my_modules/db');
 router.post('/', function(req, res, next) {
   const did = req.body.did;
 
-  db.query("SELECT q.qid, q.path, q.que FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
+  db.query("SELECT d.did, q.path, q.que FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
     console.log(rows[0]);
-    res.render('question', {qid: rows[0].qid, path: rows[0].path, que: rows[0].que});
+    res.render('question', {did: rows[0].did, path: rows[0].path, que: rows[0].que});
   });
 });
 
 router.post('/answer', function(req, res, next) {
-  const qid = req.body.qid;
+  const did = req.body.did;
   const ans = req.body.ans;
 
-  db.query("SELECT ans FROM questions_datas WHERE qid = ?;", qid, (err, rows) => {
+  db.query("SELECT d.did, q.ans FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
     console.log(rows[0]);
     if(rows[0].ans === ans){
-      res.render('answer-correct');
+      res.render('answer-correct', {did: rows[0].did});
     }else{
-      res.render('answer-incorrect');
+      res.render('answer-incorrect', {did: rows[0].did});
     }
   });
 });

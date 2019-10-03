f385fbe0301e96dba904e3f77db8d53eb8471ee6
haku-noir <hakunoir.00@gmail.com>
2019-10-03 18:51:14 +0900

    add: uidかユーザ名を渡すとスコアが返ってくるモジュールの追加

--- app/my_modules/userdata.js
+++ app/my_modules/userdata.js
@@ -3,7 +3,6 @@ var db = require('./db');
 module.exports = {
   getUid: (user) => {
     db.query("SELECT uid FROM users_datas where user=?;", user, (err, rows) => {
-      console.log(user + " " + rows[0].uid);
       return rows[0].uid;
     });
   },
@@ -11,5 +10,16 @@ module.exports = {
     db.query("SELECT user FROM users_datas where uid=?;", uid, (err, rows) => {
       return rows[0].user;
     });
+  },
+  getScore: (req) => {
+    if(typeof req == "string"){
+      db.query("SELECT s.score FROM users_datas AS d, users_scores AS s where d.uid=s.uid AND d.user=?;", req, (err, rows) => {
+        return rows[0].score;
+      });
+    }else{
+      db.query("SELECT score FROM users_scores where uid=?;", req, (err, rows) => {
+        return rows[0].score;
+      });
+    }
   }
 };

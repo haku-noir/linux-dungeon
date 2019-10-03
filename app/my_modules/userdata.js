f976e7e54bbf8aa12c0558d3548dfe019dd96803
haku-noir <hakunoir.00@gmail.com>
2019-10-03 21:23:07 +0900

    fix: userdataのモジュールを利用すると返り値がundifinedになるバグを修正
    userdataのモジュールを非同期処理に変更

--- app/my_modules/userdata.js
+++ app/my_modules/userdata.js
@@ -1,25 +1,25 @@
 var db = require('./db');
 
 module.exports = {
-  getUid: (user) => {
+  getUid: (user) => new Promise((resolve) => {
     db.query("SELECT uid FROM users_datas where user=?;", user, (err, rows) => {
-      return rows[0].uid;
+      resolve(rows[0].uid);
     });
-  },
-  getUser: (uid) => {
+  }),
+  getUser: (uid) => new Promise((resolve) => {
     db.query("SELECT user FROM users_datas where uid=?;", uid, (err, rows) => {
-      return rows[0].user;
+      resolve(rows[0].user);
     });
-  },
-  getScore: (req) => {
+  }),
+  getScore: (req) => new Promise((resolve) => {
     if(typeof req == "string"){
       db.query("SELECT s.score FROM users_datas AS d, users_scores AS s where d.uid=s.uid AND d.user=?;", req, (err, rows) => {
-        return rows[0].score;
+        resolve(rows[0].score);
       });
     }else{
       db.query("SELECT score FROM users_scores where uid=?;", req, (err, rows) => {
-        return rows[0].score;
+        resolve(rows[0].score);
       });
     }
-  }
+  })
 };

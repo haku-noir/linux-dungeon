e1877d0502742b49a0488ab8ca208d79a6e813f1
haku-noir <hakunoir.00@gmail.com>
2019-10-03 13:32:14 +0900

    add: score_triggerトリガーを追加
    ユーザがusers_datasに追加されたら、users_scoresにも追加される

--- db/sqls/users_scores.sql
+++ db/sqls/users_scores.sql
@@ -4,3 +4,10 @@ CREATE TABLE users_scores(
     uid INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     score INT NOT NULL
 );
+
+CREATE TRIGGER score_trigger
+    AFTER INSERT
+    ON users_datas FOR EACH ROW
+    INSERT INTO users_scores (score) VALUE (0);
+
+INSERT INTO users_datas (user, pass) values('test', 'test');

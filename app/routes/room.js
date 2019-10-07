b8bdf3fea4f6ab5414ed198aee8148f4c25b7450
haku-noir <hakunoir.00@gmail.com>
2019-10-07 23:56:43 +0900

    add: ユーザが部屋から出ると、部屋の位置にユーザが表示される機能の追加

--- app/routes/room.js
+++ app/routes/room.js
@@ -8,10 +8,12 @@ router.post('/', function(req, res, next) {
   const did = req.body.did;
 
   db.query("SELECT d.did, q.path, q.que FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
-    ud.getScore(user)
-      .then((score) => {
+    Promise.resolve()
+      .then(() => ud.setDid(user, did))
+      .then(() => ud.getData(user))
+      .then((data) => {
         console.log(rows[0]);
-        res.render('question', {user, score, did: rows[0].did, path: rows[0].path, que: rows[0].que});
+        res.render('question', {...data, path: rows[0].path, que: rows[0].que});
       });
   });
 });
@@ -21,19 +23,19 @@ router.post('/answer', function(req, res, next) {
   const did = req.body.did;
   const ans = req.body.ans;
 
-  db.query("SELECT d.did, q.ans, d.score FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
+  db.query("SELECT q.ans, d.score FROM dungeon_rooms AS d, questions_datas AS q WHERE d.qid=q.qid AND d.did = ?;", did, (err, rows) => {
     console.log(rows[0]);
     if(rows[0].ans === ans){
       Promise.resolve()
         .then(() => ud.addScore(user, rows[0].score))
-        .then(() => ud.getScore(user))
-        .then((score) => {
-          res.render('answer-correct', {user, score, did: rows[0].did});
+        .then(() => ud.getData(user))
+        .then((data) => {
+          res.render('answer-correct', data);
         });
     }else{
-      ud.getScore(user)
-        .then((score) => {
-          res.render('answer-incorrect', {user, score, did: rows[0].did});
+      ud.getData(user)
+        .then((data) => {
+          res.render('answer-incorrect', data);
         });
     }
   });

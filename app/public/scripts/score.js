937d8e99894da1231c7481b686c8c3cdec1fb549
nununugit <hiraganadenu@gmail.com>
2019-10-23 23:05:30 +0900

    add: modal-contentにhtmlで表示

--- app/public/scripts/score.js
+++ app/public/scripts/score.js
@@ -1,46 +1,45 @@
 $(function() {
-    $("#modal-open").click(function() {
+    $('#modal-open').click(function() {
+        console.log('aaa');
 
         $(this).blur();
-        if ($("#modal-overlay")[0]) returnfalse;
-        $("body").append('<div id="modal-overlay"></div>');
-        $("#modal-overlay").fadeIn("slow");
+        if ($('#modal-overlay')[0]) returnfalse;
+        $('body').append('<div id="modal-overlay"></div>');
+        $('#modal-overlay').fadeIn('slow');
 
 
         centeringModalSyncer();
 
 
         $.ajax({
-            url: "http://localhost/api/score",
-            type: "GET",
-            dataType: "json",
-            data: { name: 'user' },
-            success: function() {
-                let dataArray = data.any;
-
-                $.each(dataArray, function(i) {
-                    $("#mondal-content").append("<p>uid: " + dataArray[i].uid + "　user: " + dataArray[i].user + "　　score " + dataArray[i].score + ")</p>");
-                });
-            }
+            url: 'http://localhost/api/score',
+            type: 'GET',
+            dataType: 'json',
+            success: function(result) {
+                $('#modal-content').append('<s> uid: ' + result.uid + '　user: ' + result.user + 'score: ' + result.score + '</s>');
+            },
+            error: function() { console.log('Miss..'); }
         });
+
+        $('#modal-content').fadeIn('slow');
+
+
     });
 
-    $("#modal-content").fadeIn("slow");
 
-    $("#modal-overlay,#modal-close").unbind().click(function() {
-        $("#modal-content,#modal-overlay").fadeOut("slow", function() {
+    $('#modal-overlay,#modal-close').unbind().click(function() {
+        $('#modal-content,#modal-overlay').fadeOut('slow', function() {
             $('#modal-overlay').remove();
         });
     });
-});
 
-$(window).resize(centeringModalSyncer);
+    $(window).resize(centeringModalSyncer);
 
-function centeringModalSyncer() {
-    var w = $(window).width();
-    var h = $(window).height();
-    var cw = $("#modal-content").outerWidth();
-    var ch = $("#modal-content").outerHeight();
-    $("#modal-content").css({ "left": ((w - cw) / 2) + "px", "top": ((h - ch) / 2) + "px" });
-}
+    function centeringModalSyncer() {
+        var w = $(window).width();
+        var h = $(window).height();
+        var cw = $('#modal-content').outerWidth();
+        var ch = $('#modal-content').outerHeight();
+        $('#modal-content').css({ 'left': ((w - cw) / 2) + 'px', 'top': ((h - ch) / 2) + 'px' });
+    }
 });

d7f2b95c96272ca2c0790abdb85a9abdfb527bae
haku-noir <hakunoir.00@gmail.com>
2019-10-24 11:16:09 +0900

    add: ユーザが入手した宝の内容を表示する機能の追加

--- app/public/scripts/treasure.js
+++ app/public/scripts/treasure.js
@@ -1,27 +1,51 @@
 $(function(){
-  $("#modal-open").click( function(){
+  var treasurelist;
+  $.ajax({
+    url:'/api/treasurelist',
+    type:'GET',
+    data:{
+      'user':$('#user').val()
+    }
+  })
+  .done((treasures) => {
+    console.log(data);
+    treasurelist = treasures;
+    $.each(treasurelist, function(index, treasure) {
+      $('#treasure-box').append(`
+        <button id="modal-open-treasure" class="button" value="${index}">${treasure.name}</button>
+      `);
+    });
+  });
+
+  $(document).on("click", "#modal-open-treasure", function(){
+    console.log($(this).val());
     $(this).blur();
-    if($("#modal-overlay")[0])returnfalse;
-    $("body").append('<div id="modal-overlay"></div>');
-    $("#modal-overlay").fadeIn("slow");
+    if($("#modal-overlay-treasure")[0])returnfalse;
+    $("body").append('<div id="modal-overlay-treasure"></div>');
+    $("#modal-overlay-treasure").fadeIn("slow");
 
-    centeringModalSyncer();
+    centeringModalSyncerTreasure();
 
-    $("#modal-content").fadeIn("slow");
+    $("#modal-content-treasure").html(`
+      <p>${treasurelist[parseInt($(this).val())].val}<p>
+      <br>
+      <button id="modal-close-treasure" class="button">閉じる</button>
+  `);
+    $("#modal-content-treasure").fadeIn("slow");
 
-    $("#modal-overlay,#modal-close").unbind().click(function(){
-      $("#modal-content,#modal-overlay").fadeOut("slow", function(){
-        $('#modal-overlay').remove();
+    $("#modal-overlay-treasure,#modal-close-treasure").unbind().click(function(){
+      $("#modal-content-treasure,#modal-overlay-treasure").fadeOut("slow", function(){
+        $('#modal-overlay-treasure').remove();
       });
     });
   });
 
-  $(window).resize(centeringModalSyncer);
-  function centeringModalSyncer(){
+  $(window).resize(centeringModalSyncerTreasure);
+  function centeringModalSyncerTreasure(){
     var w = $(window).width();
     var h = $(window).height();
-    var cw = $("#modal-content").outerWidth();
-    var ch = $("#modal-content").outerHeight();
-    $("#modal-content").css({"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"});
+    var cw = $("#modal-content-treasure").outerWidth();
+    var ch = $("#modal-content-treasure").outerHeight();
+    $("#modal-content-treasure").css({"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"});
   }
 });

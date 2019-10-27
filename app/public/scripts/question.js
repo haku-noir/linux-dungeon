946dd99db6e472e906d8131f44c5791a4d07a2fd
haku-noir <hakunoir.00@gmail.com>
2019-10-27 11:08:35 +0900

    change: 問題文を見やすくなるように変更
    問題文を左寄せにした。
    文字のサイズを大きくした。

--- app/public/scripts/question.js
+++ app/public/scripts/question.js
@@ -9,7 +9,7 @@ $(function(){
   .done((question) => {
     var value = "";
     value += `<h1 class="text" align="center">${question.stage}</h1>`;
-    value += `<p class="text" align="center">${question.que}</p>`
+    value += `<font id="question-statement" class="text">${question.que}</font>`
     $('#question').html(value);
   });
 });

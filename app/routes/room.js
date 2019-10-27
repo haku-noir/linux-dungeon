6dd549b92bb6190f9eecba501f0ae147104f8d6a
haku-noir <hakunoir.00@gmail.com>
2019-10-27 13:32:35 +0900

    fix: 解答ページの処理に解答済かどうかの確認を追加

--- app/routes/room.js
+++ app/routes/room.js
@@ -32,21 +32,34 @@ router.post('/answer', function(req, res, next) {
   const user = req.body.user;
   const did = req.body.did;
   const ans = req.body.ans;
+  var userdata;
 
-  od.checkAnswer(did, ans)
-    .then((score) => {
-      if(score){
-        Promise.resolve()
-          .then(() => od.addAchiever(user, did))
-          .then(() => ud.addScore(user, score))
-          .then(() => ud.getData(user))
-          .then((data) => {
-            res.render('answer-correct', data);
-          });
+  Promise.resolve()
+    .then(() => ud.getData(user))
+    .then((data) => {
+      userdata = data;
+      return od.checkAchievedEvent(user, did);
+    })
+    .then((achieved) => {
+      if(achieved){
+        res.render('achieved-event', userdata);
       }else{
-        ud.getData(user)
-          .then((data) => {
-            res.render('answer-incorrect', data);
+        od.checkAnswer(did, ans)
+          .then((score) => {
+            if(score){
+              Promise.resolve()
+                .then(() => od.addAchiever(user, did))
+                .then(() => ud.addScore(user, score))
+                .then(() => ud.getData(user))
+                .then((data) => {
+                  res.render('answer-correct', data);
+                });
+            }else{
+              ud.getData(user)
+                .then((data) => {
+                  res.render('answer-incorrect', data);
+                });
+            }
           });
       }
     });
